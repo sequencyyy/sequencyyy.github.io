4dc51d7ae8a0332800f08c12a79fae8afb450ae2
Denis Epifanov <36046200+sequencyyy@users.noreply.github.com>
2025-02-14 01:23:23 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(() => {
+        document.querySelector(".photo").classList.add("visible");
+    }, 2000);
+
+    function createHeart() {
+        const heart = document.createElement("div");
+        heart.classList.add("heart");
+        heart.innerHTML = Math.random() > 0.5 ? "🤍" : "🖤"; 
+
+        heart.style.left = Math.random() * window.innerWidth + "px";
+        heart.style.top = "-10px";
+        heart.style.fontSize = Math.random() * 20 + 20 + "px";
+        heart.style.animationDuration = Math.random() * 3 + 2 + "s";
+
+        document.getElementById("hearts-container").appendChild(heart);
+
+        setTimeout(() => {
+            heart.remove();
+        }, 5000);
+    }
+
+    setInterval(createHeart, 300);
+
+    const music = document.getElementById("bg-music");
+
+
+    updateVolume(0.25); 
+
+    function playMusic() {
+        music.play().catch(error => console.log("Music error:", error));
+        document.removeEventListener("click", playMusic);
+    }
+
+    document.addEventListener("click", playMusic);
+
+    function updateVolume(newValue) {
+        music.volume = newValue; 
+    }
+});
